1ee535af25684528c593b314be16a920ffd82036
pmckenna <peter@intercom.io>
2018-04-11 15:32:33 +0100

    Return the clientWidth or clientHeight if getComputedStyle() returns null (which happens in a fairly specific case with Firefox: https://bugzilla.mozilla.org/show_bug.cgi?id=548397)

--- src/utils/innerSize.js
+++ src/utils/innerSize.js
@@ -1,6 +1,7 @@
 // Calculate height without padding.
 export function innerHeight(el) {
     const style = window.getComputedStyle(el, null);
+    if (!style) return el.clientHeight;
     return el.clientHeight -
         parseInt(style.getPropertyValue('padding-top'), 10) -
         parseInt(style.getPropertyValue('padding-bottom'), 10);
@@ -9,6 +10,7 @@ export function innerHeight(el) {
 // Calculate width without padding.
 export function innerWidth(el) {
     const style = window.getComputedStyle(el, null);
+    if (!style) return el.clientWidth;
     return el.clientWidth -
         parseInt(style.getPropertyValue('padding-left'), 10) -
         parseInt(style.getPropertyValue('padding-right'), 10);
